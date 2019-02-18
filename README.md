7b047fc166187a7e2e69444b521adc1bdec26e7f
Igor Rzegocki <igor@rzegocki.pl>
2019-02-18 15:55:42 +0100

    Initial version

---
+++ README.md
@@ -0,0 +1,11 @@
+# ajgon/dot-git
+Just a bunch of git dotfiles.
+
+## Install
+Clone and symlink or install with [ellipsis][ellipsis]:
+
+```
+$ ellipsis install ajgon/dot-git
+```
+
+[ellipsis]: http://ellipsis.sh
---
+++ ellipsis.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+config_home=${XDG_CONFIG_HOME:-$ELLIPSIS_HOME/.config}
+
+pkg.install() {
+    rm -rf "${HOME}/.gitconfig" "${HOME}/.gitignore" "${HOME}/.gitattributes"
+}
+
+pkg.link() {
+    # Link package into ~/.config/git
+    if [ ! -d "${config_home}" ]; then
+        mkdir -p "${config_home}"
+    fi
+    fs.link_file "${PKG_PATH}" "${config_home}/git"
+
+    git config --global core.pager "${config_home}/git/diff-highlight"
+}
+
+pkg.links() {
+    local files="${config_home}/git"
+
+    msg.bold "${1:-$PKG_NAME}"
+    for file in $files; do
+        local link="$(readlink "$file")"
+        echo "$(path.relative_to_packages "$link") -> $(path.relative_to_home "$file")";
+    done
+}
+
+pkg.unlink() {
+    # Remove config dir
+    rm "${config_home}/git"
+
+    # Remove all links in the home folder
+    hooks.unlink
+}
+
+pkg.uninstall() {
+    : # No action
+}
